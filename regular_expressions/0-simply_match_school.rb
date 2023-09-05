0ec26600112505785804b75b8d88b64b80524f46
KAMANZI-SERGE187 <sergekmz187@gmail.com>
2023-09-05 07:23:43 -0700

    simply match

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-def match_school(input)
-  match = input.match(/(School)/)
-
-  if match
-    matched_text = match[1]
-    puts "Match found: #{matched_text}"
-    puts "(#{matched_text.length} chars long)"
-  else
-    puts "No match found."
-  end
-end
-
-# Check if an argument is provided
-if ARGV.length == 1
-  input = ARGV[0]
-  match_school(input)
-else
-  puts "Please provide one argument to match against 'School'."
-end
+puts ARGV[0].scan(/School/).join
