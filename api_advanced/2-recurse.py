88d7280a32b735723a3f57c1adbcd4ce3db28afb
KAMANZI-SERGE187 <sergekmz187@gmail.com>
2023-11-09 02:07:28 -0800

    2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
 """
-2-main
+Function that queries the Reddit API and returns a list containing the
+titles of all hot articles for a given subreddit. If no results are
+found for the given subreddit, the function should return None.
 """
-import sys
+import requests
 
-if __name__ == '__main__':
-    recurse = __import__('2-recurse').recurse
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        result = recurse(sys.argv[1])
-        if result is not None:
-            print(len(result))
-        else:
-            print("None")
 
+def recurse(subreddit, hot_list=[], after=None):
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'after': after}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json().get('data', {})
+
+    for child in data.get('children', []):
+        hot_list.append(child['data']['title'])
+
+    after = data.get('after', None)
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+
+    return "OK" if hot_list else None
