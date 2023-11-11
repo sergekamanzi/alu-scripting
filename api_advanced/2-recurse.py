208607fc326c20fe116b03a4cada7b28e00ad834
KAMANZI-SERGE187 <sergekmz187@gmail.com>
2023-11-11 10:42:04 -0800

    api-2

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -6,27 +6,19 @@ import requests
 def recurse(subreddit, hot_list=[], after=None):
     """ returns list with titles of all hot articles in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after}
+    headers = {'User-Agent': 'MyBot/1.0'}
+    params = {"limit": 100, "after": after}
     response = requests.get(
-                                url,
-                                headers=headers,
-                                params=params,
-                                allow_redirects=False
-                            )
+                            url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
     if response.status_code == 200:
-        data = response.json().get('data')
-        if data is not None:
-            children = data.get('children')
-            if children is not None:
-                for child in children:
-                    hot_list.append(child.get('data').get('title'))
-                after = data.get('after')
-                if after is not None:
-                    return recurse(subreddit, hot_list, after)
-                else:
-                    return hot_list
-        else:
+        data = response.json()["data"]
+        hot_list += [post["data"]["title"] for post in data["children"]]
+        if data["after"] is None:
             return hot_list
-    else:
+        else:
+            return recurse(subreddit, hot_list, data["after"])
+    elif response.status_code == 404:
         return None
